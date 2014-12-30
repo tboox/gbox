8887cea5dc941490428156bea6c0def43cb9befd
ruki <waruqi@gmail.com>
2014-12-30 10:35:01 +0800

    update tbox for mac

--- pkg/tbox.pkg/inc/mac/x64/tbox.config.h
+++ pkg/tbox.pkg/inc/mac/x64/tbox.config.h
@@ -14,7 +14,7 @@
 #define TB_CONFIG_VERSION_ALTER         8
 
 // build version
-#define TB_CONFIG_VERSION_BUILD         201412261444
+#define TB_CONFIG_VERSION_BUILD         201412301031
 
 // small
 #define TB_CONFIG_SMALL                 (0)
