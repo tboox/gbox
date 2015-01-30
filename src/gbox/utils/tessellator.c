604bf78a58fb0685ef3e1a48180f51533e745bc0
ruki <waruqi@gmail.com>
2015-01-30 10:36:56 +0800

    impl make mesh for tessellator

--- src/gbox/utils/tessellator.c
+++ src/gbox/utils/tessellator.c
@@ -28,8 +28,58 @@
 #include "mesh.h"
 
 /* //////////////////////////////////////////////////////////////////////////////////////
+ * macros
+ */
+
+// the tessellator edge
+#define gb_tessellator_edge(mesh, edge)                         ((gb_tessellator_edge_ref_t)gb_mesh_edge_data(mesh, edge))
+
+// the tessellator edge winding
+#define gb_tessellator_edge_winding(mesh, edge)                 (gb_tessellator_edge(mesh, edge)->winding)
+
+// set the tessellator edge winding
+#define gb_tessellator_edge_winding_set(mesh, edge, val)        do { gb_tessellator_edge(mesh, edge)->winding = (val); } while (0)
+
+// the tessellator face
+#define gb_tessellator_face(mesh, face)                         ((gb_tessellator_face_ref_t)gb_mesh_face_data(mesh, face))
+
+// the tessellator vertex
+#define gb_tessellator_vertex(mesh, vertex)                     ((gb_tessellator_vertex_ref_t)gb_mesh_vertex_data(mesh, vertex))
+
+// the tessellator vertex point
+#define gb_tessellator_vertex_point(mesh, vertex)               (&(gb_tessellator_vertex(mesh, vertex)->point))
+
+// set the tessellator vertex point
+#define gb_tessellator_vertex_point_set(mesh, vertex, val)      do { gb_tessellator_vertex(mesh, vertex)->point = (val); } while (0)
+
+/* //////////////////////////////////////////////////////////////////////////////////////
  * types
  */
+
+// the tessellator edge type
+typedef struct __gb_tessellator_edge_t
+{
+    // the winding
+    tb_int32_t              winding;
+
+}gb_tessellator_edge_t, *gb_tessellator_edge_ref_t;
+
+// the tessellator face type
+typedef struct __gb_tessellator_face_t
+{
+    tb_size_t               dumy;
+
+}gb_tessellator_face_t, *gb_tessellator_face_ref_t;
+
+// the tessellator vertex type
+typedef struct __gb_tessellator_vertex_t
+{
+    // the point
+    gb_point_t              point;
+
+}gb_tessellator_vertex_t, *gb_tessellator_vertex_ref_t;
+
+// the tessellator impl type
 typedef struct __gb_tessellator_impl_t
 {
     // the mode
@@ -55,14 +105,93 @@ typedef struct __gb_tessellator_impl_t
 static tb_bool_t gb_tessellator_mesh_make(gb_tessellator_impl_t* impl, gb_polygon_ref_t polygon)
 {
     // check
-    tb_assert_abort(impl && impl->mesh && polygon);
+    tb_assert_abort(impl && polygon);
+
+    // the points
+    gb_point_ref_t      points = polygon->points;
+    tb_uint16_t const*  counts = polygon->counts;
+    tb_assert_abort_and_check_return_val(points && counts, tb_false);
+
+    // not exists mesh?
+    if (!impl->mesh) 
+    {
+        // init mesh
+        impl->mesh = gb_mesh_init(  tb_item_func_mem(sizeof(gb_tessellator_edge_t),     tb_null, tb_null)
+                                ,   tb_item_func_mem(sizeof(gb_tessellator_face_t),     tb_null, tb_null)
+                                ,   tb_item_func_mem(sizeof(gb_tessellator_vertex_t),   tb_null, tb_null));
+    }
+    tb_assert_abort_and_check_return_val(impl->mesh, tb_false);
 
     // clear mesh first
     gb_mesh_clear(impl->mesh);
 
-    // TODO
-    tb_trace_noimpl();
-    return tb_false;
+    // done
+    tb_bool_t           ok          = tb_true;
+    gb_point_ref_t      point       = tb_null;
+    gb_point_ref_t      point_first = tb_null;
+    tb_uint16_t         count       = *counts++;
+    tb_size_t           index       = 0;
+    gb_mesh_edge_ref_t  edge        = tb_null;
+    gb_mesh_edge_ref_t  edge_first  = tb_null;
+    while (index < count)
+    {
+        // the point
+        point = points++;
+
+        // first point?
+        if (!index) 
+        {
+            // make a non-loop edge
+            edge = gb_mesh_edge_make(impl->mesh);
+
+            // save the first point
+            point_first = point;
+
+            // save the first edge
+            edge_first = edge;
+        }
+        // closed?
+        else if (index + 1 == count && point_first->x == point->x && point_first->y == point->y)
+        {
+            // connect an edge to the first edge
+            edge = gb_mesh_edge_connect(impl->mesh, edge, edge_first);
+        }
+        else 
+        {
+            // append an edge
+            edge = gb_mesh_edge_append(impl->mesh, edge);
+        }
+
+        // check
+        tb_assert_abort_and_check_break_state(edge, ok, tb_false);
+
+        // init edge
+        gb_tessellator_edge_winding_set(impl->mesh, edge, 1);
+        gb_tessellator_edge_winding_set(impl->mesh, gb_mesh_edge_sym(edge), -1);
+        gb_tessellator_vertex_point_set(impl->mesh, gb_mesh_edge_org(edge), *point);
+
+        // next point
+        index++;
+
+        // next polygon
+        if (index == count) 
+        {
+            // next
+            count = *counts++;
+            index = 0;
+        }
+    }
+
+#ifdef __gb_debug__
+    // check mesh
+    gb_mesh_check(impl->mesh);
+#endif
+
+    // failed? clear mesh
+    if (!ok) gb_mesh_clear(impl->mesh);
+
+    // ok?
+    return ok;
 }
 static tb_void_t gb_tessellator_done_convex(gb_tessellator_impl_t* impl, gb_polygon_ref_t polygon, gb_rect_ref_t bounds)
 {
@@ -88,6 +217,9 @@ static tb_void_t gb_tessellator_done_convex(gb_tessellator_impl_t* impl, gb_poly
     // make mesh
     if (!gb_tessellator_mesh_make(impl, polygon)) return ;
 
+    // check mesh
+    tb_check_return(impl->mesh && !gb_mesh_is_empty(impl->mesh));
+
     // TODO: make triangulation
     tb_trace_noimpl();
 }
@@ -98,6 +230,9 @@ static tb_void_t gb_tessellator_done_concave(gb_tessellator_impl_t* impl, gb_pol
 
     // make mesh
     if (!gb_tessellator_mesh_make(impl, polygon)) return ;
+
+    // check mesh
+    tb_check_return(impl->mesh && !gb_mesh_is_empty(impl->mesh));
 
     // TODO
     tb_trace_noimpl();
